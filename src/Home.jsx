691130720abec8754d5079c9bba11185a1d098d4
Muttaky <muttaky9@gmail.com>
2025-11-10 09:21:07 +0600

    caro

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,28 +5,16 @@ const Home = () => {
     <div>
       <div className="carousel w-full">
         <div id="item1" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
+          <img src="/1.jpg" className="w-full" />
         </div>
         <div id="item2" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
-          />
+          <img src="/2.jpg" className="w-full" />
         </div>
         <div id="item3" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
-          />
+          <img src="/3.jpg" className="w-full" />
         </div>
         <div id="item4" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
-          />
+          <img src="/4.jpg" className="w-full" />
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
