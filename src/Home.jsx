c12b72968dee7ff94cb718b75f61d7d2466ce866
Muttaky <muttaky9@gmail.com>
2025-11-10 17:23:06 +0600

    Add6Crops

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLoaderData } from "react-router";
 
 const Home = () => {
+  const crops = useLoaderData();
+  console.log(crops);
   return (
     <div>
       <div className="carousel w-full">
@@ -32,27 +35,22 @@ const Home = () => {
         </a>
       </div>
 
-      <div>
+      <div className="grid grid-cols-3 gap-4">
         <h2>latest 6 crops</h2>
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure className="px-10 pt-10">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Card Title</h2>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
-            </p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
+        {crops.map((crop) => (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure className="px-10 pt-10">
+              <img src={crop.image} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{crop.name}</h2>
+              <p>{crop.description}</p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
 
       <div>
