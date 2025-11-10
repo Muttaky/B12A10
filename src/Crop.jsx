315a2558100fdf8d656f67ad6c8afffc45d246a3
Muttaky <muttaky9@gmail.com>
2025-11-10 21:29:19 +0600

    defaultValueDone

--- src/Crop.jsx
+++ src/Crop.jsx
@@ -25,31 +25,68 @@ const Crop = () => {
               <div className="hero bg-base-200 ">
                 <div className="hero-content flex-col">
                   <div className="text-center">
-                    <h1 className="text-5xl font-bold">Book Consultation</h1>
+                    <h1 className="text-5xl font-bold">Edit this crop</h1>
                   </div>
                   <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
                       <fieldset className="fieldset">
                         <label className="label">Name</label>
                         <input
+                          name="name"
                           type="text"
                           className="input"
-                          placeholder="Name"
-                          name="name"
-                          required
+                          defaultValue={crop.name}
                         />
-                        <label className="label">Email</label>
+                        <label className="label">Type</label>
                         <input
-                          type="email"
+                          name="type"
+                          type="text"
                           className="input"
-                          placeholder="Email"
-                          name="email"
-                          required
+                          defaultValue={crop.type}
                         />
-
-                        <button type="Submit" className="btn btn-neutral mt-4">
-                          Book Now
-                        </button>
+                        <label className="label">Price</label>
+                        <input
+                          name="price"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.price}
+                        />
+                        <label className="label">Unit</label>
+                        <input
+                          name="unit"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.unit}
+                        />
+                        <label className="label">Quantity</label>
+                        <input
+                          name="quantity"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.quantity}
+                        />
+                        <label className="label">Description</label>
+                        <input
+                          name="description"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.description}
+                        />
+                        <label className="label">Location</label>
+                        <input
+                          name="location"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.location}
+                        />
+                        <label className="label">Image</label>
+                        <input
+                          name="image"
+                          type="text"
+                          className="input"
+                          defaultValue={crop.image}
+                        />
+                        <button className="btn btn-neutral mt-4">Submit</button>
                       </fieldset>
                     </div>
                   </div>
